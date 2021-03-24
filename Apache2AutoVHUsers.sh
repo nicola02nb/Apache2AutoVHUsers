2b53167b27a7d1ed15af34c2ad868efb0d56808a
Nicola <nicola.02.nb@gmail.com>
2021-03-25 00:23:20 +0100

    Added Automation Script

---
+++ Apache2AutoVHUsers.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+echo -e -n "SELECT FUNCTION:\\n"
+echo -e -n "1) Install Service\\n"
+echo -e -n "2) Add User\\n"
+echo -e -n "3) Delete User\\n"
+echo -e -n "4) Uninstall Service\\n"
+
+echo -n "INSERT FUNCTION: "
+read -s funz
+echo -e "\\n"
+
+if [ $funz = "1" ]; then
+	apt-get install acl apache2 php libapache2-mod-php php-mysql
+	apt-get install libapache2-mpm-itk
+	a2enmod mpm_itk
+
+elif [ $funz = "2" ]; then
+	echo "Enter the username to INSERT: "
+	read username
+	if getent passwd $username > /dev/null 2>&1; then
+    		echo "ERROR: THE USER ALREADY EXISTS"
+	else
+		adduser "$username"
+		echo "USER-> $username ADDED"
+		#
+		curl https://git.e-fermi.it/s01723/apache2autovhusers/-/raw/master/userfiles.tgz?inline=false --output /home/"$username"/userfiles.tgz
+		tar xf /home/"$username"/userfiles.tgz -C /home/"$username"/
+		mv /home/"$username"/defaultuser/* /home/"$username"/
+		rm -r /home/"$username"/defaultuser
+		rm /home/"$username"/userfiles.tgz
+		#cp -r /home/defaultuser/* /home/"$username"/
+		setfacl -R -m u:"$username":rwx /home/"$username"/
+		setfacl -R -m o::--- /home/"$username"
+		curl https://git.e-fermi.it/s01723/apache2autovhusers/-/raw/master/defaultuser.conf?inline=false --output /etc/apache2/sites-available/"$username".conf
+		#cp /etc/apache2/sites-available/defaultuser.conf /etc/apache2/sites-available/"$username".conf 
+		sed -i "s/defaultuser/$username/" /etc/apache2/sites-available/"$username".conf
+		sed -i "s/defaultuser/$username/" /etc/apache2/sites-available/"$username".conf
+		a2ensite "$username"
+		systemctl reload apache2
+	fi
+elif [ $funz = "3" ]; then
+	echo -n "Enter the username to DELETE: "
+	read username
+	if getent passwd $username > /dev/null 2>&1; then
+		a2dissite "$username"
+		systemctl reload apache2
+		userdel "$username"
+		rm /etc/apache2/sites-available/"$username".conf
+	else
+		echo "ERROR: THE USER DOESN'T EXISTS"		
+	fi
+elif [ $funz = "4" ]; then
+	systemctl stop apache2
+	a2dismod mpm_itk
+	apt-get purge libapache2-mpm-itk
+	apt-get purge apache2 php libapache2-mod-php php-mysql
+	echo -e -n "SERVICE UNINSTALLED\\n"
+else 
+	echo -e -n "Bad function selected\\n"
+fi
+
